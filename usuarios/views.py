5231ad2ad43c169b2eae292c90e67f08fcc9de2e
daniel.cibolli <danielbrisch157@gmail.com>
2024-09-23 08:24:39 -0300

    feat: add logout logic

--- usuarios/views.py
+++ usuarios/views.py
@@ -53,3 +53,8 @@ def cadastro(request):
             return redirect('login')
             
     return render(request, "usuarios/cadastro.html", {'form': form})
+
+def logout(request):
+    messages.success(request, 'Deslogado com sucesso')
+    auth.logout(request)
+    return redirect('login')
